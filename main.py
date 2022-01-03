7b3776627f2e595af52b390c966d5146ec1558e8
R S <rspet123@gmail.com>
2022-01-03 00:56:07 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -7,8 +7,14 @@ import random
 import json
 import keep_alive
 import time
-tk1 = os.environ['DISC_TOKEN']
+import datetime
+import math
+import traceback
+import difflib
+tk1 = os.environ['TOKEN']
 itemdict = {}
+packaged_dict = {}
+voldict = {}
 fuzzurl1 = "https://market.fuzzwork.co.uk/aggregates/?region="
 fuzzurl2 = "&types="
 systems = {}
@@ -18,11 +24,39 @@ systems["amarr"] = "60008494"
 systems["dodixie"] = "60011866"
 systems["hek"]= "60005686"
 systems["rens"] = "60004588"
+jokes = ["Time to get a watch","Login and look, idiot","Time to get dunked on","Shouldn't you unsub your accounts?","@here Look at this pubbie"]
 url = "https://goonmetrics.apps.goonswarm.org/api/price_data/?station_id=1030049082711&type_id="
 moonwalk = ["https://www.youtube.com/watch?v=zvJskQuvypw","https://cdn.discordapp.com/attachments/768592802417999874/841529517696352306/The_escape.mp4","https://youtu.be/-UTAEk22w2A"]
 iconurl = "https://images.evetech.net/types/"
 
+def string_search(item_search, candidate_list = itemdict.keys()):
+  possible_matches = []
+  for key in candidate_list:
+    if item_search in key:
+      possible_matches.append(key)
+  return possible_matches
 
+async def item_searching(item_searched,message):
+  candidates = string_search(item_searched)
+  search_text = ""
+  for i,candidate in enumerate(candidates):
+    search_text += str(i) + " - " + candidate.title() + "\n"
+    embed=discord.Embed(title=("Searching for "+item_searched.title()), description="Jita", color=0x00fe15)
+    embed.add_field(name="Results", value=search_text, inline=True)
+    try:
+      await message.channel.send(embed=embed)
+    except discord.errors.HTTPException:
+      await message.channel.send("Too many items to display, Narrow your search please")
+      return
+    msg = await client.wait_for('message', check=check, timeout=30)
+    try:
+      index = int(msg.content)
+      itemname = candidates[index]
+    except Exception:
+      await message.channel.send("That's not right...")
+      return
+    return itemname
+  
 def format_num(number):
     out = number
     post = ""
@@ -36,17 +70,35 @@ def format_num(number):
         post = "B"
         out = number / 1000000000
     return (str(round(out, 2)) + post)
-
+def check(message):
+  #Replace this later lol
+    return True
 def comma_num(number):
-    number = round(number,0)
+    number = round(number,2)
+    if number > 1000:
+      number = int(number)
     out = "{:,}".format(number)
     return out
-
+  #Getting Packaged Volumes
+with open('invVolumes.csv', newline='') as volumes:
+  reader = csv.reader(volumes)
+  for vol in reader:
+    packaged_dict[vol[0]] = vol[1]
 
 with open('invTypes.csv', newline='') as file1:
     reader = csv.reader(file1)
     for item in reader:
         itemdict[item[2].lower()] = item[0]
+        if item[0] in packaged_dict:
+          try:
+            voldict[item[2].lower()] = float(packaged_dict[item[0]])
+          except ValueError:
+            print(item[5] + " is not a float, but thats okay")
+        else:
+          try:
+            voldict[item[2].lower()] = float(item[5])
+          except ValueError:
+            print(item[5] + " is not a float, but thats okay")
 client = discord.Client()
 print("Starting...")
 
@@ -58,6 +110,8 @@ async def on_ready():
     print("In servers: ")
     for gld in g:
       print(gld)
+      for channel in gld.text_channels:
+        print(channel)
 
 
 @client.event
@@ -69,7 +123,31 @@ async def on_message(message):
         try:
             start_time = time.time()
             itemname = message.content.split('!1dq ')[1].lower()
-            response = requests.get(url + itemdict[itemname])
+            if itemname in itemdict:
+              response = requests.get(url + itemdict[itemname])
+            else:
+              candidates = string_search(itemname)
+              search_text = ""
+              for i,candidate in enumerate(candidates):
+                search_text += str(i) + " - " + candidate.title() + "\n"
+              embed=discord.Embed(title=("Searching for "+itemname.title()), description="1DQ1-A", color=0x00fe15)
+              embed.add_field(name="Results", value=search_text, inline=True)
+              response = requests.get(url + itemdict["caracal"])
+              try:
+                await message.channel.send(embed=embed)
+              except discord.errors.HTTPException:
+                await message.channel.send("Too many items to display, Narrow your search please")
+                return
+              msg = await client.wait_for('message', check=check, timeout=30)
+              try:
+                index = int(msg.content)
+                itemname = candidates[index]
+              except Exception:
+                await message.channel.send("That's not right...")
+                return
+              response = requests.get(url + itemdict[itemname])
+              
+              
             cont = str(response.content)
             minsell = float((cont.split("<min>"))[1].split("</min>")[0])
             maxbuy = float((cont.split("<max>"))[1].split("</max>")[0])
@@ -85,8 +163,8 @@ async def on_message(message):
               await message.channel.send(itemname + " is not currently on the 1DQ1-A market")
               return
             sell1 ="Minimum: " + comma_num(minsell)
-            buy1 ="Max: " + comma_num(maxbuy)        
-            embed=discord.Embed(title=itemname, description="1DQ1-A", color=0x00fe15)
+            buy1 ="Max: " + comma_num(maxbuy)
+            embed=discord.Embed(title=itemname.title(), description="1DQ1-A", color=0x00fe15)
             embed.set_thumbnail(url=iconurl + itemdict[itemname] + "/icon")
             embed.add_field(name="Sell", value=sell1 + "ƶ", inline=True)
             embed.add_field(name="Buy", value=buy1 + "ƶ", inline=True)
@@ -97,10 +175,12 @@ async def on_message(message):
         except Exception:
             await message.channel.send("Try That Again " + message.author.name)
             print(message.author.name + " fucked up")
+            traceback.print_exc()
     if message.content.startswith('!moonwalk'):
       randint = random.randrange(0,len(moonwalk))
       print("Moonwalk #" + str(randint) + " playing")
       await message.channel.send(moonwalk[randint])
+      #JITA
     if message.content.startswith("!jita"):
       start_time = time.time()
       if message.author.name == "B" or message.author.name == "A":
@@ -108,6 +188,25 @@ async def on_message(message):
         return
       try:
         itemname = message.content.split('!jita ')[1].lower()
+        if not itemname in itemdict:
+          candidates = string_search(itemname)
+          search_text = ""
+          for i,candidate in enumerate(candidates):
+            search_text += str(i) + " - " + candidate.title() + "\n"
+          embed=discord.Embed(title=("Searching for "+itemname.title()), description="Jita", color=0x00fe15)
+          embed.add_field(name="Results", value=search_text, inline=True)
+          try:
+            await message.channel.send(embed=embed)
+          except discord.errors.HTTPException:
+            await message.channel.send("Too many items to display, Narrow your search please")
+            return
+          msg = await client.wait_for('message', check=check, timeout=30)
+          try:
+            index = int(msg.content)
+            itemname = candidates[index]
+          except Exception:
+            await message.channel.send("That's not right...")
+            return
         jitareq = requests.get(fuzzurl1 +systems["jita"] +fuzzurl2+itemdict[itemname])
         iteminf = json.loads(jitareq.text)
         
@@ -129,11 +228,16 @@ async def on_message(message):
         embed.set_thumbnail(url=iconurl + itemdict[itemname] + "/icon")
         embed.add_field(name="Sell", value=sell1, inline=True)
         embed.add_field(name="Buy", value=buy1, inline=True)
+        spread = (float(sell["min"]) - float(buy["max"]))/float(sell["min"])
+        embed.add_field(name="Bid-Ask Spread Percentage", value=str(round(spread,3)*100) + "%", inline=False)
         embed.set_footer(text="Spencer Anders' GoonBot\n" + str(round(time.time() - start_time,3) * 1000)+"ms")
         await message.channel.send(embed=embed)
       except Exception:
         await message.channel.send("Try That Again " + message.author.name)
         print(message.author.name + " fucked up")
+
+        
+        #AMARR
     if message.content.startswith("!amarr"):
       try:
         start_time = time.time()
@@ -145,7 +249,7 @@ async def on_message(message):
         sell = iteminf[itemdict[itemname]]["sell"]
         sell1 = "Minimum: " + comma_num(float(sell["min"]))+ "ƶ\nMedian: " + comma_num(float(sell["median"]))+ "ƶ\nVolume: "+ comma_num(float(sell["volume"])) + "\nOrders: " + comma_num(float(sell["orderCount"]))
         buy1 = "Maximum: " + comma_num(float(buy["max"]))+ "ƶ\nMedian: " + comma_num(float(buy["median"]))+ "ƶ\nVolume: "+ comma_num(float(buy["volume"])) + "\nOrders: " + comma_num(float(buy["orderCount"])) 
-        embed=discord.Embed(title=itemname, description="Amarr", color=0x00fe15)
+        embed=discord.Embed(title=itemname.title(), description="Amarr", color=0x00fe15)
         embed.set_thumbnail(url=iconurl + itemdict[itemname] + "/icon")
         embed.add_field(name="Sell", value=sell1, inline=True)
         embed.add_field(name="Buy", value=buy1, inline=True)
@@ -154,6 +258,9 @@ async def on_message(message):
       except Exception:
         await message.channel.send("Try That Again " + message.author.name)
         print(message.author.name + " fucked up")
+
+        
+    # MARGIN
     if message.content.startswith("!margin"):
       try:
         start_time = time.time()
@@ -165,6 +272,7 @@ async def on_message(message):
         iteminf = json.loads(amarrreq.text)
         response = requests.get(url + itemdict[itemname])
         cont = str(response.content)
+        #1DQ1-A Price
         minsell = float((cont.split("<min>"))[1].split("</min>")[0])
         maxbuy = float((cont.split("<max>"))[1].split("</max>")[0])
         move = float((cont.split("<weekly_movement>"))[1].split("</weekly_movement>")[0])
@@ -179,12 +287,13 @@ async def on_message(message):
         else:
           print(nums1)
           color1 = 0x00fe15
-        margins = str(round(((minsell- float(sell["min"]))/ minsell),3)*100)+"%"
-        embed=discord.Embed(title=itemname, description="Jita -> 1DQ1-A", color=color1)
+        margins = str(math.ceil(((minsell- float(sell["min"]))/ float(sell["min"]))*10000)/100)+"%"
+        embed=discord.Embed(title=itemname.title(), description="Jita -> 1DQ1-A", color=color1)
         embed.set_thumbnail(url=iconurl + itemdict[itemname] + "/icon")
         embed.add_field(name="1DQ1-A", value=delve, inline=True)
         embed.add_field(name="Jita", value=jita1, inline=True)
         embed.add_field(name="Sell - Sell Margin", value=margins, inline=False)
+        embed.add_field(name="Item Volume", value=(str(voldict[itemname]) + " m³"), inline=False)
         embed.set_footer(text="Spencer Anders' GoonBot\n" + str(round(time.time() - start_time,3) * 1000)+"ms")
         await message.channel.send(embed=embed)
       except Exception:
@@ -227,6 +336,9 @@ async def on_message(message):
       except Exception:
         await message.channel.send("Try That Again " + message.author.name)
         print(message.author.name + " fucked up")
+
+        
+        #COMPARE
     if message.content.startswith("!compare "):
       try:
         start_time = time.time()
@@ -238,6 +350,9 @@ async def on_message(message):
         itemname = stf1[3].lower()
         loc1 = stf1[1].lower()
         loc2 = stf1[2].lower()
+        if loc1 == loc2:
+          await message.channel.send("That's the same station dude")
+          return
         print(loc1 + "->" + loc2 + " for "+ itemname)
         json1 = requests.get(fuzzurl1 +systems[loc1] +fuzzurl2+itemdict[itemname])
         json2 = requests.get(fuzzurl1 +systems[loc2] +fuzzurl2+itemdict[itemname])
@@ -253,7 +368,7 @@ async def on_message(message):
         print("Stats 1, Done")
         stats2 = "Min Sell: " + comma_num(float(sell2["min"])) +"\nMax Buy: "+comma_num(float(buy2["max"])) + "\nVolume: " + comma_num(float(sell2["volume"]))
         print("Stats 2, Done")
-        nums1 = (float(sell2["min"])- float(sell1["min"]))/ float(sell2["min"])
+        nums1 = (math.ceil(float(sell2["min"])- float(sell1["min"]))/ float(sell2["min"])*10000)/100
         print("Margin 1, Done")
         if nums1 < 0:
           print(nums1)
@@ -261,19 +376,27 @@ async def on_message(message):
         else:
           print(nums1)
           color1 = 0x00fe15
-        margins = str(round(nums1,3)*100) + "%"
+        margins = str(round(nums1,3)) + "%"
         print("Margin 2, Done")
-        embed=discord.Embed(title=itemname.capitalize(), description=loc1.capitalize()+ " -> "+loc2.capitalize(), color=color1)
+        embed=discord.Embed(title=itemname.title(), description=loc1.capitalize()+ " -> "+loc2.capitalize(), color=color1)
         embed.set_thumbnail(url=iconurl + itemdict[itemname] + "/icon")
         embed.add_field(name=loc1.capitalize(), value=stats1, inline=True)
         embed.add_field(name=loc2.capitalize(), value=stats2, inline=True)
         embed.add_field(name="Sell - Sell Margin", value=margins, inline=False)
+        embed.add_field(name="Item Volume", value=(str(voldict[itemname]) + " m³"), inline=False)
         embed.set_footer(text="Spencer Anders' GoonBot\n" + str(round(time.time() - start_time,3) * 1000)+"ms")
         await message.channel.send(embed=embed)
       except Exception:
         await message.channel.send("Try That Again " + message.author.name)
         print(message.author.name + " fucked up")
+    if message.content.startswith('!evetime'):
+      e = datetime.datetime.now(datetime.timezone.utc).strftime("%H:%M:%S")
+      print(e)
+      randint = random.randrange(0,len(jokes))
+      await message.channel.send(jokes[randint])
+      await message.channel.send(e)
 
 if __name__ == "__main__":
   keep_alive.keep_alive()
   client.run(tk1)
+
